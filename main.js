2a7bb063e658ad8acd3a1e8d86d6bdcca2358ddd
wingyen <37422456+wingyen@users.noreply.github.com>
2018-06-05 10:58:09 +0200

    changed classnames

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ var app = new Vue({
             $(document).ready(function(){
                 $("#quicksearch").on("keyup", function() {
                   var value = $(this).val().toLowerCase();
-                  $("#book .thisBook").filter(function() {
+                  $(".thisBook .book").filter(function() {
                     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
                   });
                 });
